406c752350822a272280f7e84f5bfc1032fd4162
Jens Reiner <jens.reiner@conclurer.com>
2019-06-22 23:28:58 +0200

    init vue project with bootstrap

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Hello World</h1>
+    <b-button>Button</b-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "home"
+});
+</script>
